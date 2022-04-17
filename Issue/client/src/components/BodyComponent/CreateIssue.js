92b235bfa9cabfec55e869685b7b53fc3f09b7ba
Wahed08 <wahedfarhad123@gmail.com>
2022-04-18 02:03:03 +0600

    updated

--- Issue/client/src/components/BodyComponent/CreateIssue.js
+++ Issue/client/src/components/BodyComponent/CreateIssue.js
@@ -1,15 +1,22 @@
-import React, {useState, useContext} from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "../BodyComponent/CreateIssue.css";
-import { AuthContext } from '../Auth/auth-context';
+import { AuthContext } from "../Auth/auth-context";
+import { Button, makeStyles } from "@material-ui/core";
 
 const CreateIssue = () => {
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      marginTop: "15px",
+    },
+  }));
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const navigate = useNavigate();
   const auth = useContext(AuthContext);
-  
+  const classes = useStyles();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const post = { title, description };
@@ -25,7 +32,7 @@ const CreateIssue = () => {
           },
         });
         if (response.ok) {
-          navigate('/issue');
+          navigate("/issue");
         }
       } catch (err) {
         throw err;
@@ -34,43 +41,41 @@ const CreateIssue = () => {
   };
 
   return (
-    <div className="modal-background">
-      <div className="modal-container">
+    <div className="section">
+      <div className="container">
         <div className="title">
           <h1>Create An Issue?</h1>
         </div>
-        <div className="body">
-          <form onSubmit={handleSubmit} noValidate={true}>
-            <div className="form-group">
-              <label for="context">Context</label>
-              <input
-                type="text"
-                name="context"
-                required
-                placeholder="Enter Context"
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label for="details">Details about</label>
-              <textarea
-                type="text"
-                name="details"
-                rows={3}
-                required
-                placeholder="Enter Details"
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-            <div className="footer">
-              <button type="submit">Add</button>
-            </div>
-          </form>
-        </div>
+        <form onSubmit={handleSubmit} noValidate={true}>
+          <input
+            type="text"
+            name="context"
+            required
+            placeholder="Please Enter a Context"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            type="text"
+            name="details"
+            rows={4}
+            required
+            placeholder="Please Enter Details..."
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <div className={classes.button}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              Add
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 export default CreateIssue;
