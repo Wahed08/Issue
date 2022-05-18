8514b7b1b48be4d98c06322a068bb16d10a9f75c
Wahed08 <wahedfarhad123@gmail.com>
2022-05-18 23:45:10 +0600

    ..

--- Issue/Server/context/authentication.js
+++ Issue/Server/context/authentication.js
@@ -3,7 +3,7 @@ const HttpError = require("../ErrorModel/errorModel");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       const error = new HttpError(
         "Authentication failed, something wrong",
@@ -15,7 +15,9 @@ module.exports = (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.Secret_key);
     req.userId = { userId: decodedToken.userId };
     next();
+
   } catch (err) {
+    console.log(err);
     const error = new HttpError("Authentication failed", 401);
     return next(error);
   }
--- Issue/client/src/components/BodyComponent/IssueBody.js
+++ Issue/client/src/components/BodyComponent/IssueBody.js
@@ -1,21 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "../BodyComponent/IssueBody.css";
+import { AuthContext } from "../Auth/auth-context";
+import ErrorModal from "../BodyComponent/ShowError/ErrorModal";
 // import PostCard from "./PostCard";
 
 const IssueBody = () => {
+  
+  const auth = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState();
   let index = 1;
 
   useEffect(() => {
     const ac = new AbortController();
     const fetchPost = async () => {
       try {
-        const postData = await fetch("http://localhost:5000/api/posts");
+        const postData = await fetch("http://localhost:5000/api/posts", {
+          method: "GET",
+          headers: {
+            // "Content-Type": "application/json",
+            Authorization: "Bearer " + auth.token,
+          },
+        });
         const responseData = await postData.json();
         setPosts(responseData.All_post);
 
         if (!postData.ok) {
-          throw new Error(responseData.message);
+          setError(responseData.message);
         }
       } catch (err) {
         throw err;
@@ -23,41 +34,43 @@ const IssueBody = () => {
       return ac.abort();
     };
     fetchPost();
-  }, [posts]);
+  }, [posts, auth]);
 
   return (
-    <div className="issue-container">
-      <div className="heading">
-        <h1>All Issues</h1>
-      </div>
+    <React.Fragment>
+      <ErrorModal error = {error}/>
+      <div className="issue-container">
+        <div className="heading">
+          <h1>All Issues</h1>
+        </div>
 
-      <div className="table-container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th style={{ width: "10%" }}>No.</th>
-              <th>Issue Details</th>
-              <th style={{ width: "12%" }}>Date</th>
-              <th style={{ width: "10%" }}>Status</th>
-            </tr>
-          </thead>
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th style={{ width: "10%" }}>No.</th>
+                <th>Issue Details</th>
+                <th style={{ width: "12%" }}>Date</th>
+                <th style={{ width: "10%" }}>Status</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {posts &&
-              posts.map((post) => (
-                <tr key={index}>
-                  <td>{index++}</td>
-                  <td>{post.description}</td>
-                  <td>{post.date}</td>
-                  <td>{post.status}</td>
-                </tr>
-              ))}
+            <tbody>
+              {posts &&
+                posts.map((post) => (
+                  <tr key={index}>
+                    <td>{index++}</td>
+                    <td>{post.description}</td>
+                    <td>{post.date}</td>
+                    <td>{post.status}</td>
+                  </tr>
+                ))}
               <div className="gap"></div>
-          </tbody>
-        </table>
-      </div>
+            </tbody>
+          </table>
+        </div>
 
-      {/* <div className="card-body">
+        {/* <div className="card-body">
         {!posts.length && <div className="no-post"><h2>
           There is no Issue!
           </h2></div>}
@@ -65,7 +78,8 @@ const IssueBody = () => {
             <PostCard key = {post} title={post.title} description={post.description}/>
         ))}
       </div> */}
-    </div>
+      </div>
+    </React.Fragment>
   );
 };
 
