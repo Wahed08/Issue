1d4e0f799075fd2d402a37a5d1c4edd847ab7899
Wahed08 <wahedfarhad123@gmail.com>
2022-06-13 03:50:00 +0600

    changes

--- Issue/Server/app.js
+++ Issue/Server/app.js
@@ -18,9 +18,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/accounts", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
